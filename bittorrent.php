2e60d9b00c18c0f89c6e63f4eb5f3a990cdbee7a
acidtr1p <mrxdoes@gmail.com>
2022-02-24 00:51:29 +1100

    Update bittorrent.php

--- bittorrent.php
+++ bittorrent.php
@@ -3,6 +3,5 @@
 require_once('include/class_cache.php');
 
 $mc1 = NEW CACHE();
-
-//Credits to pdq, dj grrr, gazelle and many others....
+// Credits to pdq, dj grrr, gazelle and many others....
 ?>
